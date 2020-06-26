5f3c54001f232633d08fbcefbbd1254b4d07127e
Whyvilos <vlad03.01@mail.ru>
2020-06-26 05:33:55 +0300

    Тест cypress отдельно - 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ExpApp from './ExpApp.js';
 import '../styles/App.css';
 
-class App extends Component{  
-    render(){
-        return (
-            <div>
-                <ExpApp />
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ExpApp />
+      </div>
+    );
+  }
 }
 
 export default App;
